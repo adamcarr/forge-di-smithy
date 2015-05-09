9a627126e0b7f6d9e370dab7dbcc52d40c8f2305
Adam Carr <adam.carr@gmail.com>
2015-05-09 11:37:06 -0700

    incremental update

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="bluebird/bluebird.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="node/node.d.ts" />
